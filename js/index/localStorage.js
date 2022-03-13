1fd0c7d34b7aae221509daa5869f796652f7b7c3
TheJokeR1993 <thejokermafiaclub@gmail.com>
2022-03-13 15:10:59 +0200

    version1.1

--- js/index/localStorage.js
+++ js/index/localStorage.js
@@ -6,4 +6,4 @@ const L = {
 const localCompare = {
         get :()=> JSON.parse(localStorage.getItem(L.ARR_COMPARE)),
         set : (arr_compare) =>localStorage.setItem(L.ARR_COMPARE, JSON.stringify(arr_compare))
-} 
+} ;
